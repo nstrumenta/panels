1ede3cc2c97d7ae3d50b0511fca9ab30e646f775
Tyler Bryant <tyler@nstrumenta.com>
2025-02-28 16:22:43 +0000

    adds fzstd

--- src/mcap-support/src/decompressHandlers.ts
+++ src/mcap-support/src/decompressHandlers.ts
@@ -4,38 +4,17 @@
 
 import { McapTypes } from '@mcap/core';
 import * as lz4wasm from 'lz4-wasm';
+import { decompress } from 'fzstd';
 
 let handlersPromise: Promise<McapTypes.DecompressHandlers> | undefined;
 export async function loadDecompressHandlers(): Promise<McapTypes.DecompressHandlers> {
   return await (handlersPromise ??= _loadDecompressHandlers());
 }
 
-declare module '@foxglove/wasm-zstd/dist/wasm-zstd.wasm?init' {
-  export function decompress(buffer: Uint8Array, decompressedSize: number): Uint8Array;
-  export const isLoaded: Promise<void>;
-}
-
-declare module '@foxglove/wasm-bz2/wasm/module.wasm?init' {
-  export default function init(): Promise<void>;
-  export function decompress(
-    buffer: Uint8Array,
-    decompressedSize: number,
-    options: { small: boolean }
-  ): Uint8Array;
-}
-
 async function _loadDecompressHandlers(): Promise<McapTypes.DecompressHandlers> {
   return {
     lz4: lz4wasm.decompress,
 
-     
-    bz2: (_buffer, _decompressedSize) => {
-      throw new Error('bz2 not implemented');
-    },
-
-     
-    zstd: (_buffer, _decompressedSize) => {
-      throw new Error('zstd not implemented');
-    },
+    zstd: (buffer, _decompressedSize) => decompress(buffer),
   };
 }
